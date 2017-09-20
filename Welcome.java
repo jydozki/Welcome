a673bd3d724e74477f9844001ee571710aa64d22
jydozki <ijdm43@gmail.com>
2017-09-20 14:30:18 -0700

    Update

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i= 1; i<=10; i++)
+			System.out.println("Welcome to my world...."+ i);
+
+	}
+
+}
+
